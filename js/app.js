22029c8eaa09963362ccabbb59fb7fecf2dfd52f
zahraalqaisi <alqaisizahra9@gmail.com>
2021-06-07 23:47:28 +0300

    push lab12

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@ let firstImgIndex;
 let secondImgIndex;
 let thierdImgIndex;
 
+let productNames=[];
+let votes=[];
+let shown=[];
 
 function Product(name, source) {
     this.name=name;
@@ -23,6 +26,8 @@ function Product(name, source) {
 
 
     Product.allProducts.push(this);
+    productNames.push(this.name);
+
 }
 
 Product.allProducts=[];
@@ -48,6 +53,7 @@ new Product ('usb','img/usb.gif');
 new Product ('water-can','img/water-can.jpg');
 new Product ('wine-glass','img/wine-glass.jpg');
 
+console.log(productNames);
 
 //  console.log(Product.allProducts);
 
@@ -129,6 +135,14 @@ function handleUserClick(event) {
         // secondImgElement.removeEventListener('click',handleUserClick);
         // thierdImgElement.removeEventListener('click',handleUserClick);
         container.removeEventListener('click',handleUserClick);
+
+        for (let i = 0; i < Product.allProducts.length; i++) {
+            votes.push( Product.allProducts[i].votes);
+            shown.push( Product.allProducts[i].shown);
+            
+        }
+
+        chart();
     }
 }
 
@@ -147,5 +161,62 @@ function showinglist() {
 }
 
 
-
-
+function chart(){
+let ctx = document.getElementById('myChart');
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: productNames,
+        datasets: [{
+            label: '# of Votes',
+            data: votes,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: '# of Shown',
+            data: shown,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }
+    ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+}
